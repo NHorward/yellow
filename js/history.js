babc9a4cac51ee6c0c307302d25f6f8c00785267
Nicole Horward <nicole.horward@student.ehb.be>
2017-05-22 15:06:27 +0200

    Start van History Pagina
    Begin van de History Pagina

---
+++ js/history.js
@@ -0,0 +1,15 @@
+// Functie die JSON data ophaalt 
+// Array van data wordt toegevoegd aan var sensorData
+var sensorData; 
+$(document).ready(function () {
+ $.ajax({
+  type: "get", 
+  url: "data/data.json",
+  dataType: "json", 
+  success: function (data) {
+   console.log(data);
+   sensorData = data.day;
+   console.log(sensorData);
+  }
+ });
+});
