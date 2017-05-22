632df855c7ebb654e644dcb5494cbbd3ed2b9632
Nicole Horward <nicole.horward@student.ehb.be>
2017-05-22 15:42:28 +0200

    Balk en notification toegevoegd
    Juiste balk en notifications aan history pagina toegevoegd.

--- js/history.js
+++ js/history.js
@@ -1,19 +1,14 @@
 // Functie die JSON data ophaalt 
 // Array van data wordt toegevoegd aan var sensorData
-var sensorData; 
+var sensorData;
 $(document).ready(function () {
- console.log("1")
  $.ajax({
-  type: "get", 
-  url: "../data/data.json",
-  dataType: "json", 
-success: function (data) {
- console.log("succes");
- console.log(data);
-  sensorData = data.day;
- console.log(sensorData);
- 
+  type: "get"
+  , url: "../data/data.json"
+  , dataType: "json"
+  , success: function (data) {
+   sensorData = data.day;
+   console.log(sensorData);
   }
  });
-
 });
