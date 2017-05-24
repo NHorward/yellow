b960166c75f617f4c6417be522636fd3d0428d92
Jorik Doudelet <jorik.doudelet@student.ehb.be>
2017-05-24 22:24:26 +0200

    history

--- js/history.js
+++ js/history.js
@@ -12,7 +12,21 @@ $(document).ready(function () {
   , success: function (data) {
    sensorData = data.day;
    console.log(sensorData);
+    showTemperature();
   }
  });
 });
 
+function showTemperature() {
+  var lastValue = sensorData.length - 1;
+  var temperature = document.getElementById("tempValue");
+  var temp = document.createTextNode(parseInt(((parseInt(sensorData[0].TLiving))+(parseInt(sensorData[1].TLiving))+(parseInt(sensorData[2].TLiving))+(parseInt(sensorData[3].TLiving))+(parseInt(sensorData[4].TLiving))+(parseInt(sensorData[5].TLiving)))/sensorData.length)+"°C");
+  temperature.appendChild(temp);
+  
+  var ctx = document.getElementById("chartTemp");
+  var myTempChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options
+});
+}
